5f1f12fb41e16113284c8c9382209ab95b969a98
Omar Lizardo <olizardo@soc.ucla.edu>
2023-08-03 11:22:12 -0700

    Fixed plot lower bound problem

--- Functions/plot.preds.R
+++ Functions/plot.preds.R
@@ -1,11 +1,11 @@
-plot.preds <- function(d, xb, xlabs, yl, r.alpha = .1, p.size = 3, xm, xl) {
+plot.preds <- function(d, xb, xlabs, ym = 0, yl, r.alpha = .1, p.size = 3, xm, xl) {
    p <- ggplot(data = d, 
                mapping = aes(x = x, y = pr, fill = genre, color = genre, group = genre))
    p <- p + geom_point(size = p.size)
    p <- p + geom_line(linewidth = 0.75)
    p <- p + geom_ribbon(mapping = aes(ymin = lo, ymax = hi), 
                         alpha = r.alpha, linetype = 0)
-   p <- p + scale_y_continuous(limits = c(0, yl))
+   p <- p + scale_y_continuous(limits = c(ym, yl))
    p <- p + scale_x_continuous(limits = c(xm, xl), 
                                breaks = xb, labels = xlabs)
    p <- p + theme_minimal()
